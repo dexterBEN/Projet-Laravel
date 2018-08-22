3336e8ff699a9f201e3f4b1164cef65d521a8875
dexterBEN <bdextereric@yahoo.fr>
2018-08-22 11:16:16 +0200

    feat(server):table created, set AppServiceProvider

---
+++ projet-final/database/migrations/2018_08_21_122923_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->enum('post_type', ['stage', 'formation', 'indéfinie']);//Liste de string définis
+            $table->string('title', 100);
+            $table->text('description');//Peut être nullable
+            $table->string('date_début', 100);//Peut être nullable
+            $table->string('date_fin', 100);//Peut être nullable
+            $table->decimal('price', 7, 2);//Le 2 après la virgule signifie que l'on à deux chiffre après la virgule (Peut être nullable)
+            $table->smallInteger('maxStudent');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
---
+++ projet-final/database/migrations/2018_08_21_123551_create_category_post_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoryPostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category_post', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('post_id')->nullable();
+            $table->foreign('post_id')->references('id')->on('posts');
+            $table->unsignedInteger('category_id')->nullable();
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //Schema::dropIfExists('category_post');
+
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropForeign('category_post_category_id_foreign');
+            $table->dropColumn('category_id');
+            $table->dropForeign('category_post_post_id_foreign');
+            $table->dropColumn('post_id');   
+        });
+    }
+}
