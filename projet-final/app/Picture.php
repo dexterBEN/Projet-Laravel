6a6456b91ada50452c35d729e264792851d6940c
dexterBEN <bdextereric@yahoo.fr>
2018-08-23 16:17:15 +0200

    doc(server):add comment on code

--- projet-final/app/Picture.php
+++ projet-final/app/Picture.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Picture extends Model
 {
-    //
+    //relation picture-post has one 
     public function post(){
         return $this->belongsTo(Post::class);
     }
--- projet-final/app/Post.php
+++ projet-final/app/Post.php
@@ -6,6 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    //Permet d'envoyer les données au niveau de la BDD phpMyAdmin:
+    /*protected $fillable = [
+        'title',
+        'description',
+        'post_type',
+        'date_début',
+        'date_fin',
+        'price',
+        'maxStudent'
+    ];*/
+
     //Relation category post: one to many
     public function category(){
         return $this->belongsToMany(Category::class);
