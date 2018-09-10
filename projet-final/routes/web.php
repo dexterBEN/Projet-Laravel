2182abfce394149c9277b2da954531d4f908ce38
dexterBEN <bdextereric@yahoo.fr>
2018-09-10 09:33:05 +0200

    feat(client):update routes

--- projet-final/routes/web.php
+++ projet-final/routes/web.php
@@ -11,6 +11,22 @@
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+Auth::routes();
+
+Route::get('/', 'FrontController@index')->name('home');
+
+Route::get('post/{id}', 'FrontController@show')->name('post')->where(['id'=>'[0-9]+']);
+
+Route::get('search', 'FrontController@showResearch')->name('search');
+
+Route::get('formations', 'FrontController@showFormations')->name('formations')->where(['id'=>'[0-9]+']);
+
+Route::get('stages', 'FrontController@showStages')->name('stages')->where(['id'=>'[0-9]+']);
+
+Route::get('contact', 'FrontController@showContactForm')->name('contact');
+
+Route::resource('admin/post', 'PostController')->middleware('auth');
