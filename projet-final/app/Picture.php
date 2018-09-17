eaf2e14159cc8d8345e929d580ccbd967c2d1f6e
dexterBEN <bdextereric@yahoo.fr>
2018-09-17 23:15:53 +0200

    feat(client): update picture-post model with fillable

--- projet-final/app/Picture.php
+++ projet-final/app/Picture.php
@@ -6,7 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Picture extends Model
 {
-    //relation picture-post has one 
+    protected $fillable = [
+        'link', 'title'
+    ];
+
+    //relation picture-post, post has one picture
     public function post(){
         return $this->belongsTo(Post::class);
     }
--- projet-final/app/Post.php
+++ projet-final/app/Post.php
@@ -3,19 +3,22 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Post extends Model
 {
     //Permet d'envoyer les données au niveau de la BDD phpMyAdmin:
-    /*protected $fillable = [
+    protected $fillable = [
         'title',
         'description',
         'post_type',
         'date_début',
         'date_fin',
         'price',
-        'maxStudent'
-    ];*/
+        'maxStudent',
+        'category_id',
+        
+    ];
 
     //Relation category post: one to many
     public function category(){
@@ -25,5 +28,16 @@ class Post extends Model
     //Relation post-picture one to one
     public function picture(){
         return $this->hasOne(Picture::class);
+    }
+
+    public function getDateDebut($value){
+
+        return Carbon::parse($value)->format('d/m/Y');
+    }
+
+    public function scopeOrderByDate($query){
+        $dateActuelle = Carbon::now();
+
+        return $query->where('date_début', '>', $dateActuelle)->orderBy('date_début', 'ASC');
     }
 }
