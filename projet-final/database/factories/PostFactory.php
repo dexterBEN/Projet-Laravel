a757e0db6bd93cdb9217f022b24cd8bc9d03fef4
dexterBEN <bdextereric@yahoo.fr>
2018-09-24 13:42:40 +0200

    feat(client): add field in factory

--- projet-final/database/factories/PostFactory.php
+++ projet-final/database/factories/PostFactory.php
@@ -6,11 +6,12 @@ $factory->define(App\Post::class, function (Faker $faker) {
 
     //Pour les types de données voir la docs: https://github.com/fzaninotto/Faker#fakerproviderdatetime
     return [
-        'post_type' => $faker->randomElement(['stage' ,'formation', 'indéfinie']),
+        'post_type' => $faker->randomElement(['stage' ,'formation']),
         'title'=> $faker->sentence(),
         'description' => $faker->paragraph(),
-        'date_début' => $faker->date(),
-        'date_fin' => $faker->date(),
+        'status'=>$faker->randomElement(['published', 'unpublished']),
+        'date_début'=> $faker-> dateTimeBetween('now','1 month', 'Europe/Paris'),
+        'date_fin' => $faker-> dateTimeBetween('1 month', '3 months', 'Europe/Paris'),
         'price' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 9000.00),//6 chiffres au total par rapport à ce qui à été renseigner dans la table post
         'maxStudent' => $faker->numberBetween($min = 10, $max = 20)
 
