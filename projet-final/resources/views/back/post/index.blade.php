9eeb86553babfd22a87879495f38864d331fc5ca
dexterBEN <bdextereric@yahoo.fr>
2018-09-25 19:55:10 +0200

    feat(admin): add message success and parse date

--- projet-final/resources/views/back/post/index.blade.php
+++ projet-final/resources/views/back/post/index.blade.php
@@ -7,34 +7,38 @@
       <a href="{{route('post.create')}}" class="btn btn-primary btn-lg active">Create post</a>
     </div>
     <div class="mt-3">{{$posts->links()}}</div>
-
+    @if(Session::has('message'))
+      <div class="alert alert-info">
+        {{ Session::get('message') }}
+      </div>
+    @endif
     <table class="table">
       <thead>
         <tr>
           <th scope="col">Title</th>
+          <th scope="col">status</th>
           <th scope="col">Type de post</th>
-          <th scope="col">Date de d'apparition</th>
+          <th scope="col">Date d'apparition</th>
           <th scope="col">Date de début</th>
           <th scope="col">Date de fin</th>
           <th scope="col">Prix</th>
-          <td scope="col">edit</td>
-          <td scope="col">show</td>
+          <th scope="col">edit</th>
           <th scope="col">Delete</th>
         </tr>
       </thead>
       <tbody>
         @forelse($posts as $post)
           <tr>
-            <td>{{$post->title}}</td>
+            <td><a href="{{route('post.show', $post->id)}}">{{$post->title}}</a></td>
+            <td>{{$post->status}}</td>
             <td>{{$post->post_type}}</td>
-            <td>{{$post->created_at}}</td>
-            <td>{{$post->date_début}}</td>
-            <td>{{$post->date_fin}}</td>
+            <td>{{Carbon\Carbon::parse($post->created_at)->format('d-m-Y')}}</td>
+            <td>{{Carbon\Carbon::parse($post->date_début)->format('d-m-Y')}}</td>
+            <td>{{Carbon\Carbon::parse($post->date_fin)->format('d-m-Y')}}</td>
             <td>{{$post->price}}</td>
-            <td><a href="{{route('post.edit', $post->id)}}">edit </a></td>
-            <td><a href="{{route('post.show', $post->id)}}">show</a></td>
+            <td><a href="{{route('post.edit', $post->id)}}"><i class="fas fa-pen"></i></a></td>
             <td>
-              <form class="delete" method="POST" action="{{route('post.destroy', $post->id)}}">
+              <form onclick="return confirm('Are you sure?')" class="delete" method="POST" action="{{route('post.destroy', $post->id)}}">
                 {{ method_field('DELETE') }}
                 {{ csrf_field() }}
                 <input class="btn btn-danger" type="submit" value="delete" >
