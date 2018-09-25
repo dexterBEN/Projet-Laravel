84f041e911c00dfe2856bf90cd43eb04c808caea
dexterBEN <bdextereric@yahoo.fr>
2018-09-25 21:07:06 +0200

    refactor: remove spacing on blade

--- projet-final/resources/views/front/formations.blade.php
+++ projet-final/resources/views/front/formations.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.master')
 
 @section('content')
-
   <section class="pt-5">
     <h1>Liste des formations:</h1>
 
@@ -11,13 +10,11 @@
         <li class="list-group-item">
           <div class="container">
             <div class="row">
-
               <div class="col-sm">
                 @if(is_null($post->picture) == false) 
                   <img src="{{url('images', $post->picture->link)}}" class="img-thumbnail">
                 @endif 
               </div>
-
               <div class="col-sm">
                 <h2><a href="{{route('post', $post->id)}}">{{$post->title}}</a></h2>
                 <div class="pt-5">
@@ -25,7 +22,6 @@
                   <p>{{$post->description}}</p>
                 </div>
               </div>
-
             </div>
           </div>
         </li>
