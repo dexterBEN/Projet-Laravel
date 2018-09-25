5bdf962160d3554a8b415b9adc244888549287a7
dexterBEN <bdextereric@yahoo.fr>
2018-09-25 21:24:29 +0200

    feat(client): change show view for client

--- projet-final/resources/views/front/show.blade.php
+++ projet-final/resources/views/front/show.blade.php
@@ -3,22 +3,20 @@
 @section('content')
   <section class="mt-5">
 
-    <div class="card mx-auto" style="width: 18rem;">
-      <img class="card-img-top" src="{{url('images', $post->picture->link)}}" alt="Card image cap">
+    <div class="card mx-auto w-50 mb-5">
+      @if($post->picture)
+        <img class="card-img-top" src="{{url('images', $post->picture->link)}}" alt="Card image cap">
+      @endif
       <div class="card-body">
-        <h5 class="card-title">{{$post->title}}</h5>
+        <h5 class="card-title font-weight-bold">{{$post->title}}</h5>
         <p class="card-text">{{$post->description}}</p>
       </div>
       <ul class="list-group list-group-flush">
-        <li class="list-group-item">Date début: {{$post->date_début}}</li>
-        <li class="list-group-item">Date fin: {{$post->date_fin}}</li>
-        <li class="list-group-item">Prix: {{$post->price}}</li>
-        <li class="list-group-item">capacité d'acceuil: {{$post->maxStudent}}</li>
+        <li class="list-group-item"><p class="d-inline font-weight-bold">Date début:</p> {{Carbon\Carbon::parse($post->date_début)->format('d-m-Y')}}</li>
+        <li class="list-group-item"><p class="d-inline font-weight-bold">Date fin:</p> {{Carbon\Carbon::parse($post->date_fin)->format('d-m-Y')}}</li>
+        <li class="list-group-item"><p class="d-inline font-weight-bold">Prix:</p> {{$post->price}}</li>
+        <li class="list-group-item"><p class="d-inline font-weight-bold">capacité d'accueil:</p> {{$post->maxStudent}}</li>
       </ul>
-      <!--<div class="card-body">
-        <a href="#" class="card-link">Card link</a>
-        <a href="#" class="card-link">Another link</a>
-      </div>-->
     </div>
   </section>
 @endsection
