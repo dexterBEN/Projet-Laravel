d14c5c20c7d01cc5a3257bffe0f5836ddfdca556
dexterBEN <bdextereric@yahoo.fr>
2018-09-25 21:10:42 +0200

    feat(admin): adapt menue for admin

--- projet-final/resources/views/partials/menue-secondaire.blade.php
+++ projet-final/resources/views/partials/menue-secondaire.blade.php
@@ -1,10 +1,32 @@
 <footer>
   <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <div class="container">
       <ul class="nav">
-        <li class="nav-item active"><a class="nav-link" href="">Accueil</a></li>
-        <li class="nav-item active"><a class="nav-link" href="">Stage</a></li>
-        <li class="nav-item active"><a class="nav-link" href="">Formation</a></li>
-        <li class="nav-item active"><a class="nav-link" href="">Contact</a></li>
+        <li class="nav-item active"><a class="nav-link text-white" href="{{route('home')}}">Accueil</a></li>
+        <li class="nav-item active"><a class="nav-link text-white" href="{{route('stages')}}">Stage</a></li>
+        <li class="nav-item active"><a class="nav-link text-white" href="{{route('formations')}}">Formation</a></li>
+        @guest
+          <li class="nav-item active"><a class="nav-link text-white" href="{{route('contact')}}">Contact</a></li>
+        @else
+          
+        @endguest
       </ul>
+
+      @guest
+        
+      @else
+        <ul class="nav">
+          <li class="nav-item active"><a class="nav-link text-white" href="{{route('post.index')}}">dashboard</a></li>
+          <li class="nav-item active">
+            <a class="nav-link text-white"  href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+              Déconnexion
+            </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+              @csrf
+            </form>
+          </li>
+        </ul>
+      @endguest
+    </div>
   </nav>
 </footer>
--- projet-final/resources/views/partials/menue.blade.php
+++ projet-final/resources/views/partials/menue.blade.php
@@ -1,15 +1,38 @@
 <header>
   <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active"><a class="nav-link" href="{{route('home')}}">Accueil</a></li>
-      <li class="nav-item active"><a class="nav-link" href="{{route('stages')}}">Stage</a></li>
-      <li class="nav-item active"><a class="nav-link" href="{{route('formations')}}">Formation</a></li>
-      <li class="nav-item active"><a class="nav-link" href="{{route('contact')}}">Contact</a></li>
-    </ul>
-
-    <form class="form-inline my-2 my-lg-0"  action="{{route('search')}}" method="get">
-      <input name="search" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-primary my-2 my-sm-0" type="submit">Search</button>
-    </form>
+    <div class="container">
+      <ul class="navbar-nav mr-auto">
+        {{--Vérifie si l'administrateur est authentifier si oui on affiche pas cette partie du menue :--}}
+        
+          <li class="nav-item active"><a class="nav-link" href="{{route('home')}}">Accueil</a></li>
+          <li class="nav-item active"><a class="nav-link" href="{{route('stages')}}">Stage</a></li>
+          <li class="nav-item active"><a class="nav-link" href="{{route('formations')}}">Formation</a></li>
+          
+        @guest
+          <li class="nav-item active"><a class="nav-link" href="{{route('contact')}}">Contact</a></li>
+        @else
+          
+        @endguest
+      </ul>
+      
+      @guest
+        <form class="form-inline my-2 my-lg-0"  action="{{route('search')}}" method="get">
+          <input name="search" class="form-control mr-sm-2" type="search" placeholder="recherche" aria-label="Search">
+          <button class="btn btn-primary my-2 my-sm-0" type="submit">Rechercher</button>
+        </form>
+      @else
+        <ul class="nav">
+          <li class="nav-item active"><a class="nav-link text-white" href="{{route('post.index')}}">dashboard</a></li>
+          <li class="nav-item active">
+            <a class="nav-link text-white"  href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+              Déconnexion
+            </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+              @csrf
+            </form>
+          </li>
+        </ul>
+      @endguest
+    </div>
   </nav>
 </header>
